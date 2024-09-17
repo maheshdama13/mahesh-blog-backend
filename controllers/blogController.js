5eef5c20b8506948848c1bf634f7b6f0c568aa3e
maheshdama13 <maheshdama13@gmail.com>
2024-09-18 02:55:39 +0530

    done with improvements

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,5 +1,5 @@
-const blogService = require('../services/blogService');
-const { getIO } = require('../socket'); 
+const blogService = require("../services/blogService");
+const { getIO } = require("../socket");
 
 const getAllBlogs = async (req, res) => {
   const blogs = await blogService.getAllBlogs();
@@ -8,56 +8,64 @@ const getAllBlogs = async (req, res) => {
 
 const getBlogDetail = async (req, res) => {
   const { id } = req.params;
-  const blogDetail = await blogService.getBlogById(id, {onlyPublished: true});
-  if(blogDetail) {
+  const blogDetail = await blogService.getBlogById(id, { onlyPublished: true });
+  if (blogDetail) {
     res.json(blogDetail);
   } else {
     res.sendStatus(404);
   }
-
 };
 
 const createBlog = async (req, res) => {
-  const { name } = req.body;
-  const blog = await blogService.createBlog(name);
+  try {
+    const { name } = req.body;
+    const blog = await blogService.createBlog(name);
 
-  const blogs = await blogService.getPublishedBlogs();
+    const blogs = await blogService.getPublishedBlogs();
 
-  // Emit the event via Socket.IO
-  const io = getIO(); 
-  io.emit('update-blogs', blogs); // Send the blog post to all connected clients
-  
-  res.json(blog);
+    // Emit the event via Socket.IO
+    const io = getIO();
+    io.emit("update-blogs", blogs); // Send the blog post to all connected clients
+
+    res.json(blog);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 };
 
 const deleteBlog = async (req, res) => {
   const { id } = req.params;
-  await blogService.deleteBlog(id);
-  res.sendStatus(200);
+  try {
+    if (await blogService.deleteBlog(id)) res.sendStatus(200);
+    else res.sendStatus(500);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 };
 
 const togglePublishBlog = async (req, res) => {
-  const { id } = req.params;
-  const { isPublished } = req.body;
+  try {
+    const { id } = req.params;
+    const { isPublished } = req.body;
 
-  await blogService.togglePublishBlog(id, isPublished);
+    await blogService.togglePublishBlog(id, isPublished);
 
-  const blogs = await blogService.getPublishedBlogs();
-  // Emit the event via Socket.IO
-  const io = getIO(); 
-  io.emit('update-blogs', blogs); // Send the blog post to all connected clients
-  // res.sendStatus(200);
+    const blogs = await blogService.getPublishedBlogs();
+    // Emit the event via Socket.IO
+    const io = getIO();
+    io.emit("update-blogs", blogs); // Send the blog post to all connected clients
+    // res.sendStatus(200);
 
-
-  res.json(await blogService.getBlogById(id, {onlyPublished: false}));
-
-  
+    res.json(await blogService.getBlogById(id, { onlyPublished: false }));
+  } catch (error) {
+    res.sendStatus(500);
+  }
 };
 
-const getUserBlogs = async(req, res) => {
+const getUserBlogs = async (req, res) => {
   const blogs = await blogService.getPublishedBlogs();
   res.json(blogs);
-}
+};
 
 module.exports = {
   getAllBlogs,
@@ -65,5 +73,5 @@ module.exports = {
   createBlog,
   deleteBlog,
   togglePublishBlog,
-  getUserBlogs
+  getUserBlogs,
 };
--- controllers/postController.js
+++ controllers/postController.js
@@ -1,17 +1,17 @@
-const { default: axios } = require('axios');
-const postService = require('../services/postService');
-const path = require('path');
+const { default: axios } = require("axios");
+const postService = require("../services/postService");
+const path = require("path");
 
-const { getIO } = require('../socket'); 
-const multer = require('multer');
-const metascraper = require('metascraper')([
-  require('metascraper-author')(),
-  require('metascraper-date')(),
-  require('metascraper-description')(),
-  require('metascraper-image')(),
-  require('metascraper-title')(),
-  require('metascraper-url')(),
-])
+const { getIO } = require("../socket");
+const multer = require("multer");
+const metascraper = require("metascraper")([
+  require("metascraper-author")(),
+  require("metascraper-date")(),
+  require("metascraper-description")(),
+  require("metascraper-image")(),
+  require("metascraper-title")(),
+  require("metascraper-url")(),
+]);
 
 const getPostsByBlogId = async (req, res) => {
   const { blogId } = req.params;
@@ -22,91 +22,93 @@ const getPostsByBlogId = async (req, res) => {
 // Configure Multer for file uploads and retain the original filename
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // folder where images will be stored
+    cb(null, "uploads/"); // folder where images will be stored
   },
   filename: (req, file, cb) => {
     const originalName = file.originalname; // preserve the original name
     const ext = path.extname(originalName); // get the file extension
     const baseName = path.basename(originalName, ext); // get the base name without extension
     cb(null, `${baseName}-${Date.now()}${ext}`); // custom filename format
-  }
+  },
 });
 
 const upload = multer({ storage });
 
 const createPost = async (req, res) => {
-  const { blogId } = req.params;
-  const { title, description, facebookEmbed, twitterEmbed, metaUrl } = req.body;
-  let imageUrl = null;
+  try {
+    const { blogId } = req.params;
+    const { title, description, facebookEmbed, twitterEmbed, metaUrl } =
+      req.body;
+    let imageUrl = null;
 
-  if (req.file) {
-    // Assuming you're storing the file locally (or use services like AWS S3/Cloudinary)
-    imageUrl = `/uploads/${req.file.filename}`;
-  }
+    if (req.file) {
+      // path of the file
+      imageUrl = `/uploads/${req.file.filename}`;
+    }
 
-  let metaTitle, metaDescription, metaImage;
-  if (metaUrl) {
-    console.log(metaUrl)
-    console.log("__________metaUrl")
-    try {
+    let metaTitle, metaDescription, metaImage;
+    if (metaUrl) {
       const response = await axios.get(metaUrl);
-      // console.log(response)
-      // console.log("__________response metaUrl")
-      const metadata = await metascraper({ html: response.data, url: metaUrl });
-      console.log(metadata)
-      console.log("+++++++++++++++ metadata")
+      const metadata = await metascraper({
+        html: response.data,
+        url: metaUrl,
+      });
       metaTitle = metadata.title;
       metaDescription = metadata.description;
       metaImage = metadata.image;
-    } catch (error) {
-      console.error('Error scraping metadata:', error);
     }
+
+    const post = await postService.createPost({
+      blogId,
+      title,
+      description,
+      imageUrl,
+      facebookEmbed,
+      twitterEmbed,
+      metaUrl,
+      metaTitle,
+      metaDescription,
+      metaImage,
+    });
+
+    const posts = await postService.getPostsByBlogId(blogId);
+    // Emit the event via Socket.IO
+    const io = getIO();
+    io.emit(`update-posts-${blogId}`, posts); // Send the blog post to all connected clients
+
+    post.imageUrl = post.imageUrl ? `${process.env.APP_URL}${post.imageUrl}` : null;
+    res.json(post);
+  } catch (error) {
+    res.sendStatus(500);
   }
-
-  const post = await postService.createPost({ 
-    blogId,
-    title,
-    description,
-    imageUrl,
-    facebookEmbed,
-    twitterEmbed,
-    metaUrl,
-    metaTitle,
-    metaDescription,
-    metaImage,
-   });
-
-  const posts = await postService.getPostsByBlogId(blogId);
-  // Emit the event via Socket.IO
-  const io = getIO(); 
-  io.emit(`update-posts-${blogId}`, posts); // Send the blog post to all connected clients
-
-  res.json(post);
 };
 
 const deletePost = async (req, res) => {
   const { blogId, id } = req.params;
-  await postService.deletePost(id);
+  try {
+    await postService.deletePost(id);
 
-  const posts = await postService.getPostsByBlogId(blogId);
-  // Emit the event via Socket.IO
-  const io = getIO(); 
-  io.emit(`update-posts-${blogId}`, posts); // Send the blog post to all connected clients
+    const posts = await postService.getPostsByBlogId(blogId);
+    // Emit the event via Socket.IO
+    const io = getIO();
+    io.emit(`update-posts-${blogId}`, posts); // Send the blog post to all connected clients
 
-  res.sendStatus(200);
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 };
 
-const updatePost = async(req, res) => {
-  // try {
-    console.log("here");
+const updatePost = async (req, res) => {
+  try {
     const { title, description, facebookEmbed, twitterEmbed, metaUrl } = req.body;
     const imageUrl = req.file ? `/uploads/${req.file.filename}` : null;
 
-    console.log(imageUrl)
-    console.log("________ imageUrl")
-    
+    console.log(imageUrl);
+    console.log("________ imageUrl");
+
     const post = await postService.getPostById(req.params.id);
-    if (!post) return res.status(404).send('Post not found');
+    if (!post) return res.status(404).send("Post not found");
 
     post.title = title || post.title;
     post.description = description || post.description;
@@ -119,22 +121,22 @@ const updatePost = async(req, res) => {
 
     const posts = await postService.getPostsByBlogId(post.blogId);
     // Emit the event via Socket.IO
-    const io = getIO(); 
+    const io = getIO();
     io.emit(`update-posts-${post.blogId}`, posts);
 
     res.json(post);
-  // } catch (error) {
-    // res.status(500).send('Server error');
-  // }
-}
+  } catch (error) {
+    res.sendStatus(500);
+  }
+};
 
 // Middleware for handling image upload
-const uploadImage = upload.single('image');
+const uploadImage = upload.single("image");
 
 module.exports = {
   getPostsByBlogId,
   createPost,
   deletePost,
   updatePost,
-  uploadImage
+  uploadImage,
 };
